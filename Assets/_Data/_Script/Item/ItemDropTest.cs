66a334108d5e6f345ab27a6eac7e0617334ec000
unknown <minhnero5@gmail.com>
2025-03-04 00:02:46 +0700

    e69

--- Assets/_Data/_Script/Item/ItemDropTest.cs
+++ Assets/_Data/_Script/Item/ItemDropTest.cs
@@ -1,8 +1,13 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemDropTest : ThanguMonoBehavior
 {
     public JunkController junkController;
+    public int dropCount = 0;
+    public List<ItemDropCount> dropCountItems = new List<ItemDropCount>();
 
     protected override void Start()
     {
@@ -12,8 +17,32 @@ public class ItemDropTest : ThanguMonoBehavior
 
     protected virtual void Dropping()
     {
+        this.dropCount++; 
         Vector3 dropPos = transform.position;
         Quaternion dropRot = transform.rotation;
-        ItemDropSpawner.Instance.Drop(this.junkController.ShootableObject.dropList, dropPos, dropRot);
+        List<ItemDropRate> dropItems = ItemDropSpawner.Instance.Drop(this.junkController.ShootableObject.dropList, dropPos, dropRot);
+        ItemDropCount itemDropCount;
+        foreach (ItemDropRate itemDropRate in dropItems)
+        {
+            itemDropCount = this.dropCountItems.Find(i => i.itemName == itemDropRate.itemProfileSO.itemName);
+            if(itemDropCount == null)
+            {
+                itemDropCount = new ItemDropCount();
+                itemDropCount.itemName = itemDropRate.itemProfileSO.itemName;
+                this.dropCountItems.Add(itemDropCount);
+            }
+
+            itemDropCount.count += 1;
+            itemDropCount.rate = (float)Math.Round((float)itemDropCount.count / this.dropCount, 2) ;
+        }
     }
 }
+
+[Serializable]
+public class ItemDropCount
+{
+    public string itemName;
+    public int count;
+    public float rate;
+}
+
